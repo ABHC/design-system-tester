40d7fa8cc454a530c4da5600fcbf671fde9acc6e
ABHC <combe.alexandre@gmail.com>
2026-03-07 17:48:00 +0100

    add brand lib

---
+++ src/lib/brand/BrandABHC.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import type { Translation } from "$lib/types/translations";
+    import LogoABHC from "./LogoABHC.svelte";
+</script>
+
+<div class="brand-container">
+    <div class="tagline">
+        <div class="brand-name">
+            <h3>ALEXANDRE</h3>
+            <h3>COMBE</h3>
+        </div>
+    </div>
+    <h1 id="pipe">&nbsp;|&nbsp;</h1>
+    <div class="logo-container">
+        <LogoABHC />
+    </div>
+</div>
+
+<style>
+    .brand-container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .brand-name {
+        text-align: end;
+    }
+
+    .brand-name h3 {
+        margin: 0;
+    }
+
+    .tagline {
+        text-align: end;
+    }
+
+    .logo-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        z-index: 5;
+        height: 100%;
+        gap: 20px;
+        color: var(--accent);
+    }
+</style>
