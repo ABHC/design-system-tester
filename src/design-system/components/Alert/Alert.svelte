298c4b227d2c05d1e4748808641cf66ead1a2869
ABHC <combe.alexandre@gmail.com>
2026-03-11 01:10:36 +0100

    manage the change of variant name for included Button

--- src/design-system/components/Alert/Alert.svelte
+++ src/design-system/components/Alert/Alert.svelte
@@ -115,13 +115,13 @@
             </div>
         {:else if dismiss_label}
             <div class="alert-following">
-                <Button variant="text" size="sm" onclick={dismiss}>
+                <Button variant="textual" size="sm" onclick={dismiss}>
                     {dismiss_label}
                 </Button>
             </div>
         {:else if dismissable}
             <div class="alert-following">
-                <Button variant="text" size="sm" onclick={dismiss} aria_label="Close">
+                <Button variant="textual" size="sm" onclick={dismiss} aria_label="Close">
                     <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
                         <line x1="18" y1="6" x2="6" y2="18"></line>
                         <line x1="6" y1="6" x2="18" y2="18"></line>
