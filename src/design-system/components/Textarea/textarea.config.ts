aeb2a3894784976860d99316ab7ca9426c08c511
ABHC <combe.alexandre@gmail.com>
2026-03-26 21:11:52 +0100

    Put Textarea in its own directory with its own config file

---
+++ src/design-system/components/Textarea/textarea.config.ts
@@ -0,0 +1,23 @@
+export const TextareaConfig = {
+    base: "textarea-base",
+
+    variants: {
+        size: {
+            sm: "textarea-sm",
+            md: "textarea-md",
+            lg: "textarea-lg",
+        },
+
+        disabled: {
+            true: "textarea-disabled",
+        },
+
+        has_error: {
+            true: "textarea-has-error",
+        },
+
+        has_success: {
+            true: "textarea-has-success",
+        },
+    },
+};
