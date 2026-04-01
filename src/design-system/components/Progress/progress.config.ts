fc34cb4d47b0d76f0bf45c86c9be3246dc6992b9
ABHC <combe.alexandre@gmail.com>
2026-04-01 02:41:40 +0200

    New Progress component

---
+++ src/design-system/components/Progress/progress.config.ts
@@ -0,0 +1,39 @@
+export const progressConfig = {
+    base: "progress-base",
+
+    variants: {
+        palette: {
+            accent: "progress-fill-accent",
+            tone: "progress-fill-tone",
+            neutral: "progress-fill-neutral",
+            error: "progress-fill-error",
+            warning: "progress-fill-warning",
+            success: "progress-fill-success",
+            info: "progress-fill-info",
+        },
+
+        size: {
+            sm: "progress-sm",
+            md: "progress-md",
+            lg: "progress-lg",
+        },
+
+        elevation: {
+            none: "progress-elevation-none",
+            subtle: "progress-elevation-subtle",
+            hard: "progress-elevation-hard",
+        },
+
+        outlined: {
+            true: "progress-outlined",
+        },
+
+        rounded: {
+            true: "progress-rounded",
+        },
+
+        striped: {
+            true: "progress-striped",
+        },
+    }
+};
